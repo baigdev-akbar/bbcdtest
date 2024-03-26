d354a9af674fdf5c76545de5d67f19f4ae845b8f
Akbar Baig <48825804+akbarbaigaa@users.noreply.github.com>
2024-03-27 02:12:27 +0530

    Update Shop.jsx

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import Frame48096367 from 'Frame48096367.png';
+
 const Shop = () => {
   return (
     <div className='align-center'>
       <div className="homeUSerLogin">
-        <Link to="/user"><img src="/pictures/Frame 48096367.png" alt="" /></Link>
+        <Link to="/user"><img src={Frame48096367} alt="" /></Link>
       </div>
       <Outlet />
       <h1>Shop</h1>
