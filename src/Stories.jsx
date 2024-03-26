4e12130aaa70d31765f5dbda61deea80a8a3c8fc
Akbar Baig <48825804+akbarbaigaa@users.noreply.github.com>
2024-03-27 02:13:25 +0530

    Update Stories.jsx

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import Frame48096367 from './Frame48096367.png';
 const Stories = () => {
   return (
     <div className='align-center'>
       <div className="homeUSerLogin">
-        <Link to="/user"><img src="/pictures/Frame 48096367.png" alt="" /></Link>
+        <Link to="/user"><img src={./Frame48096367} alt="" /></Link>
       </div>
       <h1>Stories</h1>
     </div>
