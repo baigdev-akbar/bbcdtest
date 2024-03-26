7a02b74fa4e6def936c33c381accc681e169e4ad
Akbar Baig <48825804+akbarbaigaa@users.noreply.github.com>
2024-03-27 01:29:41 +0530

    Update Explr.jsx

--- src/mobileAppComps/Explr.jsx
+++ src/mobileAppComps/Explr.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { CiCircleCheck } from "react-icons/ci";
 import Buynow from '../additionalpages/Buynow';
+import checklist_2936769 from './checklist_2936769.png';
+import icon_rs from './icon_rs.png';
+import ArrowCircleRight1 from './ArrowCircleRight1.png';
+import ArrowCircleRight from './ArrowCircleRight.png';
 
 const Explr = () => {
     const byNow = () => {
@@ -15,13 +19,13 @@ const Explr = () => {
         <div className=' mob'>
             <div className='practiceMT'>
                 <div className='checklist-image'>
-                    <img src="/pictures/checklist_2936769 1.png" alt="" />
+                    <img src={checklist_2936769} alt="" />
                 </div>
                 <div className='title_phrase_text'>
                         <h3>Practice Mock Tests</h3>
                     <span>Get the authentic IELTS exam experience with all four modules covered</span>
                     </div>
-                        <button className='btnStart' onClick={start}><img src="/pictures/ArrowCircleRight.png" alt="" /></button>
+                        <button className='btnStart' onClick={start}><img src={ArrowCircleRight} alt="" /></button>
                     </div>
       <div className='expolre mob'>
            
@@ -36,8 +40,8 @@ const Explr = () => {
               <div className='subitem-img-text'><CiCircleCheck className='icon' /><span>Check Bond Scos</span></div>
             </div>
               <div className='priceButton'>
-                  <span className='priceLabel'>Only @ <img className='rupeesimg' src="/pictures/icon-rs.png" alt="" /> 299</span>
-                  <button onClick={byNow}><span>Buy Now</span> <img src="/pictures/ArrowCircleRight (1).png" alt="" /> </button>
+                  <span className='priceLabel'>Only @ <img className='rupeesimg' src={icon_rs} alt="" /> 299</span>
+                  <button onClick={byNow}><span>Buy Now</span> <img src={ArrowCircleRight1} alt="" /> </button>
               </div>
           </div>
       
