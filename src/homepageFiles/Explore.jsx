8644a11aa3bcc2755f0b6561be3aa9c49ed2d9d9
Akbar Baig <48825804+akbarbaigaa@users.noreply.github.com>
2024-03-27 02:37:48 +0530

    Update Explore.jsx

--- src/homepageFiles/Explore.jsx
+++ src/homepageFiles/Explore.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { CiCircleCheck } from "react-icons/ci";
 import Buynow from '../additionalpages/Buynow';
 import icon_rs from './icon_rs.png';
-import ArrowCircleRight1 './ArrowCircleRight1.png';
+import ArrowCircleRight1 from './ArrowCircleRight1.png';
 
 const Explore = () => {
     const byNow = () => {
@@ -20,8 +20,8 @@ const Explore = () => {
               <div className='subitem-img-text'><CiCircleCheck className='icon' /><span>Check Bond Scos</span></div>
 
               <div className='priceButton'>
-                  <span className='priceLabel'>Only @ <img className='rupeesimg' src={icon_rs.png} alt="" /> 299</span>
-                  <button onClick={byNow}><span>Buy Now</span> <img src={ArrowCircleRight1.png} alt="" /> </button>
+                  <span className='priceLabel'>Only @ <img className='rupeesimg' src={icon_rs} alt="" /> 299</span>
+                  <button onClick={byNow}><span>Buy Now</span> <img src={ArrowCircleRight1} alt="" /> </button>
               </div>
           </div>
       
