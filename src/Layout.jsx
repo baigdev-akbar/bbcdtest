4790e6156a10367e8856c80c2cefeb4ebfe6a93e
Akbar Baig <48825804+akbarbaigaa@users.noreply.github.com>
2024-03-27 02:33:59 +0530

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,6 +3,12 @@ import { NavLink, Outlet } from 'react-router-dom'
 
 import Explore from './homepageFiles/Explore'
 import LogOutBtn from './LogOutBtn'
+import logo_nobg from './logo_nobg.png'
+import home_no_bg from './home_no_bg.png'
+import Usernobg from './Usernobg.png'
+import Frame48097125 from './Frame48097125.png';
+import Storefront from './Storefront.png';
+import message_square from './message_square.png';
 
 const Layout = () => {
     
@@ -13,22 +19,22 @@ const Layout = () => {
     <div className='layout'>
             <div className='ul desk'>
             <nav className='wrapping-menu-logo'>
-                <div className='logo'><img className='logo-img' src="pictures\logo-nobg.png" alt="" /><span className='logo-text'>BabyCode</span></div>
+                <div className='logo'><img className='logo-img' src={logo_nobg} alt="" /><span className='logo-text'>BabyCode</span></div>
               
               <div className='li'>
-                  <NavLink className='manu' to='/'><img className='menu-img' src="pictures\home-no-bg.png" alt="" /><span className='menu-text' >Home</span></NavLink>
+                  <NavLink className='manu' to='/'><img className='menu-img' src={home_no_bg} alt="" /><span className='menu-text' >Home</span></NavLink>
                 </div>  
                 <div className='li'>
-                  <NavLink className='manu' to='/community'><img className='menu-img' src="pictures\Usernobg.png" alt="" /><span className='menu-text'>Community</span></NavLink>
+                  <NavLink className='manu' to='/community'><img className='menu-img' src={Usernobg} alt="" /><span className='menu-text'>Community</span></NavLink>
                 </div>
                 <div className='li'>
-                  <NavLink className='manu' to='/stories'><img className='menu-img' src="pictures\Frame 48097125.png" alt="" /><span className='menu-text'>Stories</span></NavLink>
+                  <NavLink className='manu' to='/stories'><img className='menu-img' src={Frame48097125} alt="" /><span className='menu-text'>Stories</span></NavLink>
                 </div>
                 <div className='li'>
-                  <NavLink className='manu ' to='/shop'><img className='menu-img' src="pictures\Storefront.png" alt="" /><span className='menu-text'>Shop</span></NavLink>
+                  <NavLink className='manu ' to='/shop'><img className='menu-img' src={Storefront} alt="" /><span className='menu-text'>Shop</span></NavLink>
                 </div>
                 <div className='li'>
-                  <NavLink className='manu' to='/feedback'><img className='menu-img' src="pictures\message-square.png" alt="" /><span className='menu-text'>Feedback</span></NavLink>
+                  <NavLink className='manu' to='/feedback'><img className='menu-img' src={message_square} alt="" /><span className='menu-text'>Feedback</span></NavLink>
                 </div>
                 </nav>
                 <div className='bottom-module'> <Explore /></div>
@@ -39,22 +45,22 @@ const Layout = () => {
         
         <div className='ul mob '>
             <nav className='wrapping-menu-logo'>
-                <div className='logo desk'><img className='logo-img' src="pictures\logo-nobg.png" alt="" /><span className='logo-text'>BabyCode</span></div>
+                <div className='logo desk'><img className='logo-img' src={logo_nobg} alt="" /><span className='logo-text'>BabyCode</span></div>
               
               <div className='li'>
-                  <NavLink className='manu' to='/'><img className='menu-img' src="pictures\home-no-bg.png" alt="" /><span className='menu-text' >Home</span></NavLink>
+                  <NavLink className='manu' to='/'><img className='menu-img' src={home_no_bg} alt="" /><span className='menu-text' >Home</span></NavLink>
                 </div>  
                 <div className='li'>
-                  <NavLink className='manu' to='/community'><img className='menu-img' src="pictures\Usernobg.png" alt="" /><span className='menu-text'>Community</span></NavLink>
+                  <NavLink className='manu' to='/community'><img className='menu-img' src={Usernobg} alt="" /><span className='menu-text'>Community</span></NavLink>
                 </div>
                 <div className='li'>
-                  <NavLink className='manu' to='/stories'><img className='menu-img' src="pictures\Frame 48097125.png" alt="" /><span className='menu-text'>Stories</span></NavLink>
+                  <NavLink className='manu' to='/stories'><img className='menu-img' src={Frame48097125} alt="" /><span className='menu-text'>Stories</span></NavLink>
                 </div>
                 <div className='li'>
-                  <NavLink className='manu ' to='/shop'><img className='menu-img' src="pictures\Storefront.png" alt="" /><span className='menu-text'>Shop</span></NavLink>
+                  <NavLink className='manu ' to='/shop'><img className='menu-img' src={Storefront} alt="" /><span className='menu-text'>Shop</span></NavLink>
                 </div>
                 <div className='li'>
-                  <NavLink className='manu' to='/feedback'><img className='menu-img' src="pictures\message-square.png" alt="" /><span className='menu-text'>Feedback</span></NavLink>
+                  <NavLink className='manu' to='/feedback'><img className='menu-img' src={message_square} alt="" /><span className='menu-text'>Feedback</span></NavLink>
                 </div>
                 </nav>
                 <div className='bottom-module desk'> <Explore /></div>
